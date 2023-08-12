494ac4f511ba9c07981d576e481430171e483e01
Farah Abdi <90638797+bigboyfreezy@users.noreply.github.com>
2023-08-12 09:30:50 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,19 +1,25 @@
-// create web server with express
-// express is a node.js module for building HTTP servers
-const express = require('express');
-const router = express.Router();
-const comments = require('../data/comments');
-
-router.get('/comments', (req, res) => {
-    res.json(comments);
+// Create web server
+var express = require('express');
+var app = express();
+// Create server
+var server = require('http').createServer(app);
+// Create socket
+var io = require('socket.io').listen(server);
+// Create array to store comments
+var comments = [];
+// Set static folder
+app.use(express.static(__dirname + '/public'));
+// Listen to port 3000
+server.listen(3000);
+// Connect to socket
+io.sockets.on('connection', function(socket) {
+	// Send comments to client
+	socket.emit('load comments', comments);
+	// Add comment
+	socket.on('add comment', function(data) {
+		// Push comment to array
+		comments.push(data);
+		// Send comment to all clients
+		io.sockets.emit('add comment', data);
+	});
 });
-
-router.get('/comments/:id', (req, res) => {
-    const found = comments.some(comment => comment.id === parseInt(req.params.id));
-    if (found) {
-        res.json(comments.filter(comment => comment.id === parseInt(req.params.id)));
-    } else {
-        res.status(400).json({msg: `No comment with the id of ${req.params.id}`});
-    }
-});
-
